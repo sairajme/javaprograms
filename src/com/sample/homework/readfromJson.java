fa1dcb42ee961d59424f554176579ef10d43083d
Sairaj <sairajme@gmail.com>
2020-07-14 00:19:55 +0530

    new changes

--- src/com/sample/homework/readfromJson.java
+++ src/com/sample/homework/readfromJson.java
@@ -2,42 +2,50 @@ package com.sample.homework;
 
 import java.io.FileReader;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-public class readfromJson extends SearchCSV{
+public class readfromJson extends SearchCSV {
 
-	
 	public readfromJson() {
 		// TODO Auto-generated constructor stub
 		try {
-			
-	         Object obj = new JSONParser().parse(new FileReader("C:\\Users\\Win10\\Desktop\\sample.json"));
-	         JSONObject jsonObject = (JSONObject) obj;
-	         
-	         String Name= (String) jsonObject.get("Name");
-	         setName(Name);
-	         String Subject = (String)jsonObject.get("Subject");
-	         setSubject(Subject);
-	         long Regno= (long) jsonObject.get("RegNo");
-	         setRollNo((int) Regno);
-	         long MaxMarks = (long)jsonObject.get("MaxMarks");
-	         setMaxMarks((int) MaxMarks);
-	         long ScoredMarks= (long) jsonObject.get("ScoredMarks");
-	         setSubjectMarks((int) ScoredMarks);
-	        
-	         System.out.println("Names===>  "+getName());
-	         System.out.println("Subject===>  "+getSubject());
-	         System.out.println("Roll No===>  "+getRollNo());
-	         System.out.println("Subject===>  "+getSubjectMarks());
-	         System.out.println("Max Marks===>  "+getMaxMarks());
-	         
-	        
-	      } catch(Exception e) {
-	         e.printStackTrace();
-	      }
+
+			Object obj = new JSONParser().parse(new FileReader("C:\\Users\\Win10\\Desktop\\sample.json"));
+			JSONObject jsonObject = (JSONObject) obj;
+
+			String Name = (String) jsonObject.get("Name");
+			setName(Name);
+			long Regno = (long) jsonObject.get("RegNo");
+			setRollNo((int) Regno);
+			long MaxMarks = (long) jsonObject.get("MaxMarks");
+			setMaxMarks((int) MaxMarks);
+
+			System.out.println("Names===>  " + getName());
+			System.out.println("Roll No===>  " + getRollNo());
+			System.out.println("Max Marks===>  " + getMaxMarks());
+
+			JSONArray ja = (JSONArray) jsonObject.get("Marks");
+			Iterator itr2 = ja.iterator();
+			while (itr2.hasNext()) {
+				JSONObject markobj = (JSONObject) itr2.next();
+				Long sum = 0l;
+				for (Object subname : markobj.keySet()) {
+
+					System.out.println(subname + "==" + markobj.get(subname));
+					Long mark = (Long) markobj.get(subname);
+					sum = Long.sum(sum, mark);
+					mark = sum;
+				}
+				System.out.println(sum);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
